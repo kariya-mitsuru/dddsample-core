0b46646a9ac21d8fbb3fb7f7c16190356ecab9fe
Mitsuru Kariya <kariya_mitsuru@hotmail.com>
2020-01-16 18:47:12 +0900

    Use lombok

--- src/main/java/com/pathfinder/api/TransitEdge.java
+++ src/main/java/com/pathfinder/api/TransitEdge.java
@@ -1,5 +1,7 @@
 package com.pathfinder.api;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -8,6 +10,8 @@ import java.util.Date;
  * describing the route of a cargo.
  *  
  */
+@Getter
+@RequiredArgsConstructor
 public final class TransitEdge implements Serializable {
 
   private final String edge;
@@ -15,45 +19,4 @@ public final class TransitEdge implements Serializable {
   private final String toNode;
   private final Date fromDate;
   private final Date toDate;
-
-  /**
-   * Constructor.
-   *
-   * @param edge
-   * @param fromNode
-   * @param toNode
-   * @param fromDate
-   * @param toDate
-   */
-  public TransitEdge(final String edge,
-                     final String fromNode,
-                     final String toNode,
-                     final Date fromDate,
-                     final Date toDate) {
-    this.edge = edge;
-    this.fromNode = fromNode;
-    this.toNode = toNode;
-    this.fromDate = fromDate;
-    this.toDate = toDate;
-  }
-
-  public String getEdge() {
-    return edge;
-  }
-
-  public String getFromNode() {
-    return fromNode;
-  }
-
-  public String getToNode() {
-    return toNode;
-  }
-
-  public Date getFromDate() {
-    return fromDate;
-  }
-
-  public Date getToDate() {
-    return toDate;
-  }
 }
